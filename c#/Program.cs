42360cb79b4db53f031d50a949a928e6e4ab6ad7
Nigel Doheny <nigel.doheny93@gmail.com>
2023-03-09 11:53:34 +0000

    Variables in C#

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,18 +1,46 @@
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 using System;
 
-class HelloWorld{
-    static void Main(string[] args){
-       int num = 55;
-       int num1 = 55;
-       string name = "Nigel";
-       Console.WriteLine("Hello, World!");
-       Console.WriteLine(num);
-       Console.WriteLine(name);
-       Console.WriteLine(num + num1);
-       Console.WriteLine(num * num1);
-       Console.WriteLine(num / num1);
-       Console.WriteLine(num - num1);
+namespace CodeSample
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            // 10 is an interger
+            // 10.5 is a decimal
+            // 'c' is a single charater
+            // "Friend" is a word of 6 charaters i.e string
+        int A = 10; 
+        int B = -50; 
+        int C = 0; 
+        int A1; // assigning value for variable after it has been declared.
+        int B1; 
+        A1 = 15; 
+        B1 = -35; 
+        int A2; 
+        int B2; 
+        A2 = 15; 
+        B2 = A2; 
+        int A3; 
+        int B3; 
+        A3 = 15;
+        B3 = -A3;
+
+        int num = 55;
+        int num1 = 55;
+        string name = "Nigel";
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(num);
+        Console.WriteLine(name);
+        Console.WriteLine(num + num1);
+        Console.WriteLine(num * num1);
+        Console.WriteLine(num / num1);
+        Console.WriteLine(num - num1);
+        Console.WriteLine(B2);
+        Console.WriteLine(B3);
+        Console.WriteLine("{0} {1}",B3, A3);
+        }
     }
 }
--- c#/learnC#.cs
+++ c#/learnC#.cs
@@ -1,8 +1,23 @@
+/*
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk
+*/
 // dotnet new console
 // dotnet build 
 // dotnet run
 
+/* 
+gitpod /workspace/programminghub (main) $ cd /workspace/programminghub/c#
+gitpod /workspace/programminghub/c# (main) $ dotnet build 
+*/
 
-/*
-is this multiline
+/* 
+We use Integer type of variables to store whole numbers – keyword “int”
+For storing the Decimal type of numbers, we use decimal variables – keyword “double”
+If we want to store a single character, we use character variables – keyword “char”
+In the case of storing words or sentences. we use the String variables - keyword “string”
 */
