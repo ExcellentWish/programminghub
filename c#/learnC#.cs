3a1fd10dcc0ac55dbefeda7000180af6eb483d13
Nigel Doheny <nigel.doheny93@gmail.com>
2023-03-28 11:31:15 +0000

    Update C#

--- c#/learnC#.cs
+++ c#/learnC#.cs
@@ -4,8 +4,9 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
-sudo apt-get install -y dotnet-sdk
+sudo apt-get install -y dotnet-sdk-7.0
 */
+
 // dotnet new console
 // dotnet build 
 // dotnet run
@@ -20,4 +21,7 @@ We use Integer type of variables to store whole numbers – keyword “int”
 For storing the Decimal type of numbers, we use decimal variables – keyword “double”
 If we want to store a single character, we use character variables – keyword “char”
 In the case of storing words or sentences. we use the String variables - keyword “string”
+*/
+/*
+sudo apt-cache search dotnet-sdk search for the SDK
 */
